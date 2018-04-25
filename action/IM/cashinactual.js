8f6d206c27041cc4f90196f2506ab674e8f16a40
Andersen <andersen.cheng@yonyou.com.hk>
2018-04-25 19:06:23 +0800

    排序

--- action/IM/cashinactual.js
+++ action/IM/cashinactual.js
@@ -7,16 +7,32 @@ const wb = require('../../util/webutils');
 const moment = require('moment');
 const appContext = require('../../ctx/appContext');
 const Constants = require('../../ctx/constants').Constants;
-const {CashinActual,Project} = require('../../domain/model');
+const {CashinActual,Project,Contract} = require('../../domain/model');
 const logger = require("../../util/logger").log;
 const {ObjectId} = require('mongodb');
 
 router.post("/listCashinActual",function(req,res){
     let page = wb.getPagination(req);
     let q={};
+    let contractId1;
+    let contractId2 =null;
     if (wb.containsQueryParam(req,"q","contractId"))
     {
-        q["contractId"]= {$regex: req.body.q.contractId,$options:'i'};
+
+        let contractId1 = req.body.q.contractId;
+            orm.find((new Contract()).getCollection(),{contractId: contractId1},null,function(err,contracts){
+            if(err)
+            {
+                model.err=err;
+            }
+            else {
+                //model.contact=contracts;
+                contractId2 = contracts[0]._id;
+                q["contractId"] =contractId2;
+            }
+        });
+
+
     }
 
     orm.pagejoinquery(new Project(),["pm","contractId"],q,page,function(err,rs){
@@ -29,26 +45,46 @@ router.post("/listCashinActual",function(req,res){
 
 router.post("/saveCashinActual",function(req,res){
   let item = req.body.CashinActual;
+  let contract1=req.body.proj.contract._id;
+  console.log(item);
+
+ //  console.log(req.body);
+  //console.log(req);
   let depot ={};
-  let contract1 = req.body.contract;
    db.getDb(function(err,connection){
         if(err){
             model.err=err;
         }
       else{
+          let p = new Contract();
           let ca= new CashinActual();
-          ca.contractId = ObjectId(item.contractId);
-          ca.projectId =  ObjectId(item.projectId);
-          ca.amount =  parseFloat(item.amount);
+          ca.contractId = req.body.proj.contract._id;
+       // console.log(ca.contractId);
+          ca.projectId =  req.body.proj._id;
+       //   console.log(ca.projectId);
+          ca.amount =  parseFloat(req.body.proj.contract.amount);
           ca.currency =  parseInt(item.currency);
           ca.cash =  parseInt(item.cash);
           ca.cashindate =  item.cashindate;
           ca.entrydate = item.entrydate;
+          orm.get(p.getCollection(),ca.contractId,function(err,rs){
+                Object.assign(p,rs);
+
+              p.accReceiptAmt = ca.cash + p.accReceiptAmt;
+
+              orm.update(p,req.body.proj.contract._id,function(rs){
+                  console.log(rs);
+              });
+            });
+
           db.insertOne(ca.getCollection(), ca, function (err, rs) {
               depot[rs._id]=rs;
               res.json({CashinActual:rs});
           }, connection);
+
+
       }
+
 
   })
 });
