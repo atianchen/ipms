fd3a5ac20216e6245b277f4e5bad6eaba45ce450
Andersen <andersen.cheng@yonyou.com.hk>
2018-05-04 11:49:40 +0800

    日期查询

--- action/IM/accrualActual.js
+++ action/IM/accrualActual.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 const appContext = require('../../ctx/appContext');
 const Constants = require('../../ctx/constants').Constants;
 const projectStatus = require('../../db/constants').projectStatus;
-const {AccrualActual,Project,Contract} = require('../../domain/model');
+const {AccrualActual,Project,Contract,Person} = require('../../domain/model');
 const logger = require("../../util/logger").log;
 const {ObjectId} = require('mongodb');
 
@@ -20,21 +20,63 @@ router.post("/listAccrualActual",function(req,res){
         q["projectId"]= {$regex: req.body.q.projectId, $options:'i'};
         let project1 = req.body.q.projectId;
     }
-    orm.pagejoinquery(new Project(),["pmId"],q,page,function(err,rs){
-        let model = {};
-        model.page=page;
-        model.data=rs;
-        res.json(model);
+    let model={};
+    async.waterfall([
+        (cb)=>{
+            if (wb.containsQueryParam(req,"q","pmId"))
+            {
+                orm.find((new Person()).getCollection(),{userId:{$regex: req.body.q.pmId.trim(), $options:'i'}},null,(err,rs)=>
+                {
+                    q.pmId = {$in:rs.map((x)=>{return x._id;})};
+                    cb(err);
+                });
+            }
+            else {
+                cb(null);
+            }
+        },
+        (cb)=>{
+            orm.find((new Project()).getCollection(),{},{projectId:1},function(err,projects){
+                model.projects=projects;
+                //console.log(projects);
+                cb(err);
+            });
+        },
+         (cb)=>{
+            orm.find((new Person()).getCollection(),{_id:{$in:model.projects.map((q)=> q.pmId)}},{userId:1},function(err,persons){
+                model.persons=persons;
+                cb(err);
+            });
+        },
+        (cb)=>{
+        orm.pagejoinquery(new Project(),["pmId"],q,page,function(err,rs){
+            model.page=page;
+            model.data=rs;
+            cb(err);
+        });
+        }
+    ], function (err, result) {
+        if (err)
+            res.json({err:err});
+        else
+        {
+            res.json(model);
+        }
     });
+
+    // orm.pagejoinquery(new Project(),["pmId"],q,page,function(err,rs){
+    //     let model = {};
+    //     model.page=page;
+    //     model.data=rs;
+    //     res.json(model);
+    // });
 });
 router.post("/saveAccrualActual",function(req,res){
     let item = req.body.AccrualActual;
-    //console.log(item);
     let depot ={};
     let test1 =req.body;
     let project1 = req.body.proj;
     let milestone1 = req.body.AccrualActual.completemilestone;
-   //console.log(project1);
     db.getDb(function(err,connection){
         if(err){
             model.err=err;
@@ -43,17 +85,20 @@ router.post("/saveAccrualActual",function(req,res){
             let ca= new AccrualActual();
             let p = new Project();
             ca.projectId =  req.body.proj._id;
-            console.log(ca.projectId);
             ca.completemilestone =  item.completemilestone;
-            console.log(ca.completemilestone);
             ca.accrual = parseFloat(item.accrual);
             ca.confirmdate =  item.confirmdate;
             ca.entrydate = item.entrydate;
             orm.get(p.getCollection(),ca.projectId,function(err,rs){
                     Object.assign(p,rs);
                     p.completedMilestone =ca.completemilestone;
-                   // console.log(p.accComAccrual);
-                    p.accComAccrual= p.accComAccrual+ parseFloat(ca.accrual);
+                   console.log(p.accComAccrual);
+                   if(p.accComAccrual==null)
+                   {
+                       console.log(p.accComAccrual);
+                       p.accComAccrual = parseFloat(0);
+                   }
+                    p.accComAccrual= p.accComAccrual + parseFloat(ca.accrual);
                    // console.log(typeof(p.currentYearAccAccrual));
                     //console.log(typeof(ca.accrual));
                     p.currentYearAccAccrual = parseFloat(p.currentYearAccAccrual) + parseFloat(ca.accrual);
